bedd9dc8e38fe29a4544a7e5569145ac11e0855f
israt merin <isratmarin02@gmail.com>
2023-12-03 15:18:29 +0600

    first commit

--- src/Components/Header/Menubar/Menubar.jsx
+++ src/Components/Header/Menubar/Menubar.jsx
@@ -13,12 +13,12 @@ export const Menubar = () => {
 <div><h1>Logo</h1></div>
 
 
-<div className={`${toggle? "":"hidden"} flex flex-col  mt-[43px] pb-[30px] md:pb-0 md:block md:m-0 text-center w-96 `}>
+<div className={`${toggle? "transition-all ":"hidden"} flex flex-col  mt-[43px] pb-[30px] md:pb-0 md:block md:m-0 text-center w-96  duration-700 ease-linear`}>
     
-<NavLink to="/"className="no-underlie  mx-2 hover:bg-sky-700 hover: block md:inline-block">Home</NavLink>
-<NavLink to="/services" className="no-underlie  mx-2  block md:inline-block">Services</NavLink>
-<NavLink to="/contact" className="no-underlie mx-2 block md:inline-block">Contact</NavLink>
-<NavLink to="/about" className="no-underlie mx-2 block md:inline-block ">About</NavLink>
+<NavLink to="/"className="no-underlie mx-2 block md:inline-block hover:bg-[gray]">Home</NavLink>
+<NavLink to="/services" className="no-underlie  mx-2  block md:inline-block hover:bg-[gray]">Services</NavLink>
+<NavLink to="/contact" className="no-underlie mx-2 block md:inline-block hover:bg-[gray]">Contact</NavLink>
+<NavLink to="/about" className="no-underlie mx-2 block md:inline-block hover:bg-[gray]">About</NavLink>
 </div>
 
 <div className='md:hidden'>
